c5546e1e9d74b91d55ca6c627abbaa93c5bbb2e2
Morgan Klockars <morganklockars@gmail.com>
2023-04-20 15:51:54 +0000

    add Structure and questions and answers for the quiz game

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+questions = ("What is the highest-grossing R-rated movie of all time?: ",
+                       "What 1994 crime film revitalized John Travolta's career?: ",
+                       'In Apocalypse Now, Robert Duvall says, "I love the smell of ___ in the morning.": ',
+                       'Which Alfred Hitchcock thriller is notorious for its shocking "shower scene"??: ',
+                       "How many suns does Luke's home planet of Tatooine have in Star Wars?: ")
+
+options = (("A. Joker", "B. Blade", "C. Dark Knight", "D. Django Unchained"),
+                   ("A. Grease", "B. Two of a kind", "C. Pulp Fiction", "D. A civil action"),
+                   ("A. Diesel", "B. Napalm", "C. Gun Powder", "D. Rotten eggs"),
+                   ("A. Notorius!", "B. Frenzy", "C. Marnie", "D. Psycho"),
+                   ("A. 5", "B. 2", "C. 7", "D. 3"))
+
