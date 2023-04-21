27ec3b38b50b0b5ee3e73135278cc6d055f288df
Morgan Klockars <morganklockars@gmail.com>
2023-04-21 12:21:26 +0000

    fixed indentation

--- run.py
+++ run.py
@@ -4,21 +4,20 @@ The touples will store the variables i will need later in the code.
 
 """
 
-
 questions = ("What is the highest-grossing R-rated movie of all time?: ",
-            "What 1994 crime film revitalized John Travolta's career?: ",
-            'In Apocalypse Now, Robert Duvall says, \
-                 "I love the smell of ___ in the morning.": ',
-            'Which Alfred Hitchcock thriller is notorious for \
-                 its shocking "shower scene"??: ',
-            "How many suns does Luke's home planet of \
-                 Tatooine have in Star Wars?: ")
+               "What 1994 crime film revitalized John Travolta's career?: ",
+               'In Apocalypse Now, Robert Duvall says, \
+               "I love the smell of ___ in the morning.": ',
+               'Which Alfred Hitchcock thriller is notorious for \
+               its shocking "shower scene"??: ',
+               "How many suns does Luke's home planet of \
+               Tatooine have in Star Wars?: ")
 
 options = (("A. Joker", "B. Blade", "C. Dark Knight", "D. Django Unchained"),
-            ("A. Grease", "B. Two of a kind", "C. Pulp Fiction", "D. A civil action"),
-            ("A. Diesel", "B. Napalm", "C. Gun Powder", "D. Rotten eggs"),
-            ("A. Notorius!", "B. Frenzy", "C. Marnie", "D. Psycho"),
-            ("A. 5", "B. 2", "C. 7", "D. 3"))
+          ("A. Grease", "B. Two of a kind", "C. Pulp Fiction", "D. A civil action"),
+          ("A. Diesel", "B. Napalm", "C. Gun Powder", "D. Rotten eggs"),
+          ("A. Notorius!", "B. Frenzy", "C. Marnie", "D. Psycho"),
+          ("A. 5", "B. 2", "C. 7", "D. 3"))
 
 """
 4 variables set to the correct values to be used later.
@@ -30,24 +29,33 @@ question number is set to  0.
 """
 
 
-ANSWERS = ("A", "C", "B", "D", "B")
+answers = ("A", "C", "B", "D", "B")
 guesses = []
 score = 0
 question_num = 0
 
 
-for question in questions:
-    print("----------------------")
-    print(question)
-    for option in options[question_num]:
-        print(option)
+"""
 
-    guess = input("Enter (A, B, C, D): ").upper()
+Main function that has for a loop to get the questions from the tuple.
+Then prints them, we also have a for loop that get the options from the other
+tuple and prints them to the terminal. THen we have an input that asks you 
+to input A,B,C,D that is corresponding to the guesses.
+We also have a if statment that asks if the guess is equal to the 
+correct answer and if it's not.
+
+"""
+
+for questions in questions:
+    print("...")
+    print(question)
+    for options in options[question_num]:
+       print(option)
+
+    guess = input("PICK ONE (A,B,C,D) THEN PRESS ENTER:").upper()
     guesses.append(guess)
     if guess == answers[question_num]:
         score += 1
-        print("CORRECT!")
+    print("CORRECT")
     else:
-        print("INCORRECT!")
-        print(f"{answers[question_num]} is the correct answer")
-    question_num += 1
+        print(f"INCORRECT {answers[question_num]} IS THE CORRECT ANSWER!")
